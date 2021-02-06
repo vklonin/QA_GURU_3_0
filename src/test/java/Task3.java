1c840325737aaf52cdc599004cf8847a3a2c1c43
vladimir klonin <vklonin@gmail.com>
2021-02-06 19:50:17 +0400

    2 task corrected, 3 task re-written

--- src/test/java/Task3.java
+++ src/test/java/Task3.java
@@ -2,6 +2,9 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.Coordinates;
+
+import java.awt.*;
 
 import static com.codeborne.selenide.Condition.cssClass;
 import static com.codeborne.selenide.Condition.text;
@@ -15,8 +18,18 @@ public class Task3 {
 
     void checkSoftAssertion(){
 
+
+        Robot robot = null;
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+
+        //String browserPosition = Configuration.browserPosition;
+
         Configuration.timeout = 3000;
-        Configuration.startMaximized = true;
+        Configuration.startMaximized = true; // надо для локатора мышки через робот
 
         // open  link
         open("https://the-internet.herokuapp.com/drag_and_drop");
@@ -30,8 +43,14 @@ public class Task3 {
         // find column B
         SelenideElement columnB = $("#column-b");
 
+        Coordinates coordinates = columnB.getCoordinates();
+        int offsetX = 5; // смещение внутрь прямоугольника
+        int offsetY = 110; //смещение от угла экрана до окна браузера
+
 
         //sleep(4000);
+
+        robot.mouseMove(coordinates.onPage().x+offsetX, coordinates.onPage().y+offsetY);
 
         actions().dragAndDrop(columnA, columnB).build().perform();
 
